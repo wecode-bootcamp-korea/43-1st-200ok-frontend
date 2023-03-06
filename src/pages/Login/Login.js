de7afbd506c975811e1d6c0b48cf3b489771a7cb
frlampard <cskr1013@gmail.com>
2023-03-06 22:07:04 +0900

    ADD: 로그인 페이지 기능 및 디자인 작업 중

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import './Login.scss';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
 
   const [isActive, setIsActive] = useState(false);
 
@@ -12,11 +15,33 @@ const Login = () => {
       ? setIsActive(true)
       : setIsActive(false);
   };
+  const Login = e => {
+    e.preventDefault();
+
+    fetch('', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json;charset=utf-8' },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    }) //요청
+      .then(response => response.json())
+      .then(data => console.log(data));
+    //응답
+
+    if (isActive) {
+      navigate('/');
+    }
+  };
 
   return (
-    <div className="login">
+    <section className="login">
       <form className="loginForm">
-        <div className="loginInputBox">
+        <div>
+          <img className="mainLogo" src="images/logo.png" alt="mainLogo" />
+        </div>
+        <div>
           <input
             className="idInput"
             type="text"
@@ -30,23 +55,32 @@ const Login = () => {
             onKeyUp={isInputLogin}
           />
           <span>
-            <button className="loginButton">로그인 버튼</button>
+            <button
+              className={isActive ? 'activeButton' : 'unActiveButton'}
+              type="button"
+              disabled={email === '' || password === '' ? true : false}
+              onClick={Login}
+            >
+              로그인 버튼
+            </button>
           </span>
-          <input type="checkbox" />
-          <label>
-            <span>이메일 저장</span>
-          </label>
-          <input className="signUp" type="button" />
-          <label>
-            <span>회원가입</span>
-          </label>
-          <input className="lostAccounts" type="button" />
-          <label>
-            <span>이메일/비밀번호 찾기</span>
-          </label>
+          <div>
+            <div className="loginEtc">
+              <input className="saveEmail" type="checkbox" />
+              <label>
+                <span>이메일 저장</span>
+              </label>
+            </div>
+            <button className="signUp" type="button">
+              회원가입
+            </button>
+            <button className="lostAccounts" type="button">
+              이메일/비밀번호 찾기
+            </button>
+          </div>
         </div>
       </form>
-    </div>
+    </section>
   );
 };
 
