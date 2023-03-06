04d93b9f46fabb4c15faefbe2b9f4ba3ff418d0d
zunyange <hiillun00553300@gmail.com>
2023-03-06 17:38:47 +0900

    ADD : 이미지 폰트 추가

--- 200ok-frontend/src/Router.js
+++ 200ok-frontend/src/Router.js
@@ -8,7 +8,7 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/main" element={<Main />} />
+        <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
---
+++ 200ok-frontend/src/pages/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.scss';
+
+const Main = () => {
+  return (
+    <>
+      <div>안녕하세요</div>
+      <div className="ggg">asdsd</div>
+    </>
+  );
+};
+
+export default Main;
