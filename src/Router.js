6c900bc1d71f4a82b0d1e5b0bb71a27945563bca
shin <hole546@naver.com>
2023-03-07 18:39:37 +0900

    ADD: 메인 페이지 구성

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
 import Main from './pages/Main/Main';
+import Footer from './components/Footer/Footer';
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
