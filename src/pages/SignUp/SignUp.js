b02d75f350955d62cb28c07a26db39ef06d4b06c
zunyange <hiillun00553300@gmail.com>
2023-03-07 20:42:26 +0900

    [43기 김준영] ADD : 회원가입 페이지 구현 완료

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,9 +1,289 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import './SignUp.scss';
 
 const SignUp = () => {
+  const [getIsActive, setGetIsActive] = useState(false);
+  const [inputValue, setInputValue] = useState({
+    userName: '',
+    email: '',
+    password: '',
+    passwordCheck: '',
+    phoneNumber: '',
+  });
+  const { userName, email, password, passwordCheck, phoneNumber } = inputValue;
+  const handleInput = event => {
+    const { name, value } = event.target;
+    setInputValue({ ...inputValue, [name]: value });
+  };
+
+  //유효성 검사
+  const isValidEmail = email.includes('@', 4) && email.includes('.com');
+  const isValidPassWord = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?]).{8,}$/;
+
+  // const specialLetter = password.search(/[!@#$%]/gi);
+  // const numberList = password.search(/[0-9]/gi);
+  // const isValidPassword =
+  //   password.length >= 8 && specialLetter.length >= 1 && numberList.length >= 1;
+
+  // 가입하기 버튼 활성화
+  // 이름과 핸드폰번호의 value가 1자 이상이 되어야 한다
+  const isValidInput = userName.length >= 1 && phoneNumber.length >= 11;
+
+  // 검사한 모든 로직의 유효성 검사가 true가 될때 getIsActive함수가 작동한다. 버튼 클릭 이벤트가 발생할때 넣어줄 함수.
+  // const activeButton = () => {
+  //   if (isValidEmail && isValidInput && isValidPassWord) {
+  //     setGetIsActive(true);
+  //   } else {
+  //     alert('양식에 맞춰서 다시 입력해주세요.');
+  //     setGetIsActive(false);
+  //   }
+  // };
+
+  const navigate = useNavigate();
+  const goToLogin = event => {
+    event.preventDefault();
+    if (isValidEmail && isValidInput && isValidPassWord) {
+      navigate('/signupterms');
+    } else {
+      alert('양식에 맞춰서 다시 입력해주세요.');
+    }
+  };
+
+  const activeBtn = () => {
+    return isValidEmail && isValidInput && isValidPassWord
+      ? setGetIsActive(true)
+      : setGetIsActive(false);
+  };
+  // 버튼이 비활성화 될 때 버튼을 클릭하면 아래와 같은 경고창이 뜬다.
+
+  // backend 연결 - activeButton 함수 안에 넣기
+  // fetch(``, {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json;charset=utf-8',
+  //   },
+  //   body: JSON.stringify({
+  //     name: userName,
+  //     email: email,
+  //     password: password,
+  //     passwordCheck: passwordCheck,
+  //     phoneNumber: phoneNumber,
+  //   }),
+  // })
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     alert('200OK에서 즐거운 쇼핑 되세요♡♥︎♡♥︎');
+  //     navigate('/login');
+  //   });
+
   return (
-    <div className="signup">
-      <p>asds</p>
+    <div className="signUp">
+      <div className="signUpHeader">
+        <Link to="/">
+          <img className="signUpLogo" src="images/logo.png" alt="signUpLogo" />
+        </Link>
+        <span className="signUpTitle">회원가입</span>
+        <hr className="signUpLine" />
+      </div>
+      <div className="signUpWrap">
+        <div className="infoFirst">
+          <img
+            className="signUpCircle"
+            src="images/signup-circle.png"
+            alt="signUpCircle"
+          />
+          <span className="signUpInfoTitle">기본정보</span>
+        </div>
+        <div className="infoFirstContent">
+          <table summary="이름, 이메일, 비밀번호, 비밀번호 재입력, 생년월일, 핸드폰번호">
+            <tbody>
+              <tr>
+                <th scope="row">
+                  <div className="infoTitle">
+                    이름
+                    <span className="ns" title="필수입력">
+                      {' '}
+                      *
+                    </span>
+                  </div>
+                </th>
+                <td>
+                  <div className="inputWrap">
+                    <input
+                      type="text"
+                      className="inputInfo"
+                      title="이름 입력"
+                      name="userName"
+                      value={userName}
+                      onChange={handleInput}
+                    />
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">
+                  <div className="infoTitle">
+                    이메일
+                    <span className="ns" title="필수입력">
+                      {' '}
+                      *
+                    </span>
+                  </div>
+                </th>
+                <td id="td_webId">
+                  <div className="inputWrap">
+                    <input
+                      type="text"
+                      className="inputInfo"
+                      title="이메일 입력"
+                      name="email"
+                      value={email}
+                      placeholder="   예 : 200OKKK@spao.com"
+                      onChange={handleInput}
+                    />
+                  </div>
+                  {isValidEmail ? (
+                    <em className="emailFormCorrect">올바른 형식입니다.</em>
+                  ) : (
+                    <em className="emailForm">양식에 맞춰 입력해주세요.</em>
+                  )}
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">
+                  <div className="infoTitle">
+                    비밀번호
+                    <span className="ns" title="필수입력">
+                      {' '}
+                      *
+                    </span>
+                  </div>
+                </th>
+                <td id="td_webId">
+                  <div className="inputWrap">
+                    <input
+                      type="password"
+                      className="inputInfo"
+                      title="비밀번호 입력"
+                      name="password"
+                      value={password}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div id="em_pwdValidMsg" />
+                  {isValidPassWord.test(password) ? (
+                    <em className="passwordFormCorrect">올바른 형식입니다.</em>
+                  ) : (
+                    <em className="passwordForm">
+                      영문/숫자/특수문자 조합으로 8자리 이상
+                    </em>
+                  )}
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">
+                  <div className="infoTitle">
+                    비밀번호 재입력
+                    <span className="ns" title="필수입력">
+                      {' '}
+                      *
+                    </span>
+                  </div>
+                </th>
+                <td id="td_confirmPassword">
+                  <div className="inputWrap">
+                    <input
+                      type="password"
+                      className="inputInfo"
+                      title="비밀번호 재입력"
+                      name="passwordCheck"
+                      value={passwordCheck}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div id="em_pwdValidMsg2" />
+                  {password === passwordCheck ? (
+                    <em className="pwCheckFormCorrect">
+                      비밀번호가 일치합니다.
+                    </em>
+                  ) : (
+                    <em className="pwCheckForm">동일한 비밀번호를 입력</em>
+                  )}
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">
+                  <div className="infoTitle">생년월일</div>
+                </th>
+                <td id="td_confirmPassword">
+                  <div className="inputWrap">
+                    <input
+                      type="text"
+                      className="inputInfo"
+                      title="생년월일"
+                      placeholder="   예 : 1900/01/01"
+                    />
+                  </div>
+                  <div id="em_pwdValidMsg2" />
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">
+                  <div className="infoTitle">
+                    핸드폰번호
+                    <span className="ns" title="필수입력">
+                      {' '}
+                      *
+                    </span>
+                  </div>
+                </th>
+                <td id="td_confirmPassword">
+                  <div className="inputWrap">
+                    <input
+                      type="text"
+                      className="inputInfo"
+                      title="핸드폰번호"
+                      placeholder="   예 : 01012345678"
+                      name="phoneNumber"
+                      value={phoneNumber}
+                      onChange={handleInput}
+                      onKeyUp={activeBtn}
+                    />
+                  </div>
+                  <div id="em_pwdValidMsg2" />
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">
+                  <div className="infoTitle">주소</div>
+                </th>
+                <td id="td_confirmPassword">
+                  <div className="inputWrap">
+                    <input type="text" className="inputInfo" title="주소" />
+                  </div>
+                  <div id="em_pwdValidMsg" />
+                  <em className="addressForm">도로명 주소를 입력해주세요.</em>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="signUpYesOrNo">
+          <button className="btnCancelSignup">
+            <span>가입취소</span>
+          </button>
+          <button
+            className={
+              getIsActive ? 'signUpButtonAction' : 'signUpButtonInaction'
+            }
+            disabled={!getIsActive}
+            onClick={goToLogin}
+          >
+            가입하기
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/pages/SignUp/SignUpTerms.js
@@ -0,0 +1,196 @@
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import './SignUpTerms.scss';
+
+const SignUpTerms = () => {
+  const [isAllChecked, setAllChecked] = useState(false);
+  const [checkedState, setCheckedState] = useState(new Array(2).fill(false));
+  const navigate = useNavigate();
+  const handleAllCheck = () => {
+    setAllChecked(prev => !prev);
+    let array = new Array(2).fill(!isAllChecked);
+    setCheckedState(array);
+  };
+
+  const handleMonoCheck = position => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCheckedState(updatedCheckedState);
+    const checkedLength = updatedCheckedState.reduce((sum, currentState) => {
+      if (currentState === true) {
+        return sum + 1;
+      }
+      return sum;
+    }, 0);
+    setAllChecked(checkedLength === updatedCheckedState.length);
+  };
+  const goToSignup = event => {
+    event.preventDefault();
+    if (checkedState[0]) {
+      navigate('/signup');
+    } else {
+      alert('필수약관에 동의해주셔야 합니다!');
+    }
+  };
+
+  // const goToMain = event => {
+  //   if (id.includes('@') && pw.length >= 5) {
+  //     fetch('http://10.58.52.56:3000/auth/signin', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json;charset=utf-8',
+  //       },
+  //       body: JSON.stringify({
+  //         email: id,
+  //         password: pw,
+  //       }),
+  //     })
+  //       .then(response => response.json())
+  //       .then(data => {
+  //         if (data.accessToken) {
+  //           localStorage.setItem('token', data.accessToken);
+  //         } else {
+  //           alert('아이디와 비밀번호를 다시 확인해주세요!');
+  //         }
+  //       });
+  //   }
+  // };
+
+  // const activeBtn = () => {
+  //   return checkedState[0] ? setCheckedState(true) : setCheckedState(false);
+  // };
+
+  return (
+    <div className="signUpTerms">
+      <div className="signUpHeader">
+        <Link to="/">
+          <img className="signUpLogo" src="images/logo.png" alt="signUpLogo" />
+        </Link>
+        <span className="signUpTitle">회원가입</span>
+        <hr className="signUpLine" />
+      </div>
+      <div className="termsWrap">
+        <div className="allCheck">
+          <span className="customCheck">
+            <input
+              type="checkbox"
+              className="checkBox"
+              checked={isAllChecked}
+              onChange={() => handleAllCheck()}
+            />
+          </span>
+          <label className="allCheckTxt">
+            모든 약관을 확인하고 전체동의합니다.
+          </label>
+        </div>
+        <hr className="signUpTermsLine" />
+        <div className="termsContentWrap">
+          <div className="termsFirstAgree">
+            <span className="customCheck">
+              <input
+                type="checkbox"
+                className="checkBox"
+                checked={checkedState[0]}
+                onChange={() => handleMonoCheck(0)}
+              />
+              <label className="firstCheckTxt">개인정보 수집 동의 [필수]</label>
+            </span>
+          </div>
+          <div className="termsFirstTxt" summary="개인정보 수집, 이용동의">
+            *&nbsp;
+            <strong>수집 항목 [필수] : </strong>
+            <br />
+            <b>
+              &nbsp;&nbsp;&nbsp;&nbsp;아이디, 비밀번호, 성명, 핸드폰번호,
+              이메일, 본인인증정보(CI)
+            </b>
+            <br />
+            *&nbsp;
+            <strong>이용목적 : </strong>
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;1. 회원관리 - 200OK 서비스 이용에 따른 본인
+            확인, 개인식별, 불량회원의 부정이용 방지와 비인가 사용 방지,
+            가입의사 확인 가입 및 가입횟수 제한, 분쟁 조정을 위한 기록보존,
+            불만처리 등 민원처리, 고지사항 전달
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;2. 서비스 제공에 관한 계약이행 - 이랜드
+            멤버십 포인트적립, 사용. 금융거래 본인인증 및 금융서비스, 구매 및
+            요금결제.
+            <br />
+            *&nbsp;
+            <strong>
+              보유/이용기간 : 회원 탈퇴 요청 시 지체 없이 삭제, 파기 됩니다.(단,
+              회원가입 시 혜택을 목적으로 한 반복적인 가입, 탈퇴를 막기 위해
+              일정기간(30일 이내) 보관 될 수 있습니다.)
+            </strong>
+            <br />
+            *&nbsp;
+            <strong>
+              개인정보 필수 수집항목 수집/이용동의를 거부하실 수 있으나 동의
+              거부 시 회원가입을 하실 수 없습니다.
+            </strong>
+          </div>
+          <div className="termsSecondAgree">
+            <span className="customCheck">
+              <input
+                type="checkbox"
+                className="checkBox"
+                checked={checkedState[1]}
+                onChange={() => handleMonoCheck(1)}
+              />
+              <label className="secondCheckTxt">
+                개인정보 수집 동의 [선택]
+              </label>
+            </span>
+          </div>
+          <div className="termsFirstTxt" summary="개인정보 수집, 이용동의">
+            *&nbsp;
+            <strong>수집 항목 [선택] : </strong>
+            <br />
+            <b>&nbsp;&nbsp;&nbsp;&nbsp;주소, 생년월일</b>
+            <br />
+            *&nbsp;
+            <strong>이용목적 : </strong>
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;경품 및 쇼핑물품 배송에 대한 정확한 배송지
+            확보, 생일쿠폰, 200OK 멤버십 활용 및 안내정보 제공. 당사가 진행하는
+            서비스, 행사 및 제휴행사의 안내.
+            <br />
+            *&nbsp;
+            <strong>
+              보유/이용기간 : 회원 탈퇴 요청 시 지체 없이 삭제, 파기 됩니다.(단,
+              회원가입 시 혜택을 목적으로 한 반복적인 가입, 탈퇴를 막기 위해
+              일정기간(30일 이내) 보관 될 수 있습니다.)
+            </strong>
+            <br />
+            *&nbsp;
+            <strong>
+              고객님께서는 선택항목 개인정보 수집/이용 동의 거부시, 상기
+              이용목적에 명시된 서비스를 받으실 수 없습니다.
+            </strong>
+          </div>
+        </div>
+      </div>
+      <div className="signUpYesOrNo">
+        <button onClick={() => navigate('/')} className="btnCancelSignup">
+          <span>가입취소</span>
+        </button>
+        <button
+          className={
+            (checkedState[0] && checkedState[1]) || checkedState[0]
+              ? 'termButtonAction'
+              : 'termButtonInAction'
+          }
+          disabled={!checkedState}
+          onClick={goToSignup}
+          // keyup={activeBtn}
+        >
+          다음으로
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpTerms;
