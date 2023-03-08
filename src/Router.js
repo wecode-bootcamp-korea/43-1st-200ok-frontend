e71eaf479506d003409253f7386eb6e2ef996603
shin <hole546@naver.com>
2023-03-08 15:42:27 +0900

    ADD: Nav 페이지 구현

--- src/Router.js
+++ src/Router.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
-import Main from './pages/Main/Main';
-import Footer from './components/Footer/Footer';
-import Login from './pages/Login/Login';
-import SignUp from './pages/SignUp/SignUp';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Nav />
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<Nav />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 };
