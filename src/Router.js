a0b87075c5f69d6a5c80c43dea6b566af5e1975a
shin <hole546@naver.com>
2023-03-08 20:17:52 +0900

    ADD: 피드백 수정 완료

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main/Main';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
