d7fcdba4c2264e93b02e4d5f57a17e96ed455b98
frlampard <cskr1013@gmail.com>
2023-03-09 18:36:45 +0900

    ADD: 제품 상세페이지 디자인 변경

--- src/pages/DetailProduct/DetailProduct.js
+++ src/pages/DetailProduct/DetailProduct.js
@@ -19,26 +19,30 @@ const DetailProduct = () => {
           </div>
         </section>
         <aside className="infoArea">
-          <div className="productName">[제품명 | 제품코드]</div>
-          <div className="productPrice">제품 가격(50,000 원)</div>
+          <div className="productName">
+            [램스울100 라운드넥 스웨터 | C,M_SPKWD23U99]
+          </div>
+          <div className="priceArea">
+            <div className="productPrice">제품 가격 (50,000 원)</div>
+            <div className="productSalePrice">할인 가격 (10% 할인)</div>
+          </div>
           <div className="productCount">(-) 제품 수량 (+)</div>
           <div className="productColor">
-            <p>[색상 선택]</p>
+            <p>[Color]</p>
             <button className="productColor1">red</button>
             <button className="productColor2">blue</button>
             <button className="productColor3">green</button>
-            <button className="productColor4">yellowgreen</button>
+            <button className="productColor4">purple</button>
           </div>
           <div className="productSize">
-            <p>[사이즈 선택]</p>
+            <p>[Size]</p>
             <button className="productSize1">95</button>
             <button className="productSize2">100</button>
             <button className="productSize3">105</button>
             <button className="productSize4">110</button>
           </div>
           <div className="sumPrice">
-            <p>[총 제품금액]</p>
-            <div>(100,000 원)</div>
+            <p>[총 제품금액] (100,000 원)</p>
           </div>
           <div className="buttons">
             <button>
