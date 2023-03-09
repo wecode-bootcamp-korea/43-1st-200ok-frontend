3b01de4a437522101634ecf6554d7f0baee7ce67
shin <hole546@naver.com>
2023-03-09 14:10:19 +0900

    ADD: 라우터 경로 수정

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
-import SignUp from './pages/SignUp/SignUp';
+import SignUp from './pages/SignUp/SiginUp';
 import Footer from './components/Footer/Footer';
 
 const Router = () => {
