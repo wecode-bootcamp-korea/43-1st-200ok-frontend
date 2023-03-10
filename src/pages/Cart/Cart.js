29271a6fb768b9614bdfeb3662b4f1ef80182a1d
zunyange <hiillun00553300@gmail.com>
2023-03-10 10:37:51 +0900

    ADD : 장바구니 페이지 디자인 완성

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,15 +1,54 @@
 import React, { useState } from 'react';
 import './Cart.scss';
 import Count from '../../components/Count/Count';
-// import Cartitem from './Cartitem/Cartitem';
+import CartList from '../../components/CartList/CartList';
 
-const Cart = () => {
+const Cart = props => {
   const [count, setCount] = useState(1);
-  const [product, setProduct] = useState([]);
   const [price, setPrice] = useState('5000');
-
   const totalPrice = price * count;
+  const [isAllChecked, setAllChecked] = useState(false);
+  const [checkedState, setCheckedState] = useState(new Array(2).fill(false));
+  const [saveOrder, setSaveOrder] = useState([]);
+  const [productList, setProductList] = useState([]);
 
+  const handleDeleteSelected = () => {
+    const selectedProductIds = isAllChecked;
+    setProductList(prevProducts =>
+      prevProducts.filter(
+        product => !selectedProductIds.includes(product.cartId)
+      )
+    );
+    setAllChecked([]);
+  };
+
+  const handleAllCheck = () => {
+    setAllChecked(prev => !prev);
+    let array = new Array(2).fill(!isAllChecked);
+    setCheckedState(array);
+  };
+
+  const handleMonoCheck = position => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCheckedState(updatedCheckedState);
+    const checkedLength = updatedCheckedState.reduce((sum, currentState) => {
+      if (currentState === true) {
+        return sum + 1;
+      }
+      return sum;
+    }, 0);
+    setAllChecked(checkedLength === updatedCheckedState.length);
+  };
+
+  const deleteProduct = item => {
+    setSaveOrder(
+      saveOrder.filter(item => {
+        return item.id !== item.id;
+      })
+    );
+  };
   return (
     <div className="cart">
       <div className="cartHeader">
@@ -22,13 +61,27 @@ const Cart = () => {
       <div className="cartList">
         <div className="ListContent">
           <div className="cardHeader">
-            <input className="cartAllCheckBox" type="checkbox" />
-            <label className="cartAllCheck">전체선택</label>
+            <div className="AllCheckBox">
+              <input
+                className="cartAllCheckBox"
+                type="checkbox"
+                checked={isAllChecked}
+                onChange={() => handleAllCheck()}
+              />
+              <label className="cartAllCheck">전체선택</label>
+            </div>
+
+            <button className="cartAllDelete">선택상품 삭제</button>
           </div>
           <div className="cartProductInfo">
             <div className="cartCard">
               <div className="ProductInfo">
-                <input type="checkbox" class="cartCheckBox" />
+                <input
+                  type="checkbox"
+                  class="cartCheckBox"
+                  checked={checkedState[1]}
+                  onChange={() => handleMonoCheck(1)}
+                />
                 <img
                   className="productImg"
                   src="images/cartproduct.png"
@@ -104,6 +157,16 @@ const Cart = () => {
 
 export default Cart;
 
+// const deleteComment = item => {
+//   setSaveComment(
+//     saveComment.filter(items => {
+//       return items.id !== item.id;
+//     })
+//   );
+//   setNumberComments(numberComments - 1);
+//   return item.up === true ? setNumberLike(numberlike - 1) : '';
+// };
+
 const PRODUCT_LIST = [
   {
     id: 1,
@@ -122,11 +185,6 @@ const PRODUCT_LIST = [
   },
   {
     id: 4,
-    title: '포켓 트러커 자켓_SPJKD32G08',
-    price: 500,
-  },
-  {
-    id: 5,
     title: '포켓 트러커 자켓_SPJKD32G08',
     price: 500,
   },
