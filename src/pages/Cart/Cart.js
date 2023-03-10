a1d0b1edcf01d89000cd95261ef0ffab958c98f1
zunyange <hiillun00553300@gmail.com>
2023-03-10 15:12:10 +0900

    ADD : 네스팅 중

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -52,16 +52,16 @@ const Cart = props => {
   return (
     <div className="cart">
       <div className="cartHeader">
-        <div className="path">
+        <div className="cartPath">
           <span className="pathHome">HOME</span>
           <span className="pathCart">장바구니</span>
         </div>
         <h1 className="cartTitle">장바구니</h1>
       </div>
       <div className="cartList">
-        <div className="ListContent">
+        <div className="listContent">
           <div className="cardHeader">
-            <div className="AllCheckBox">
+            <div className="allCheckBox">
               <input
                 className="cartAllCheckBox"
                 type="checkbox"
@@ -81,6 +81,38 @@ const Cart = props => {
                   class="cartCheckBox"
                   checked={checkedState[1]}
                   onChange={() => handleMonoCheck(1)}
+                />
+                <img
+                  className="productImg"
+                  src="images/cartproduct.png"
+                  alt="cart.img"
+                />
+                <div className="productDetail">
+                  <strong>포켓 트러커 자켓_SPJKD32G08</strong>
+                  <p>[옵션: (10)WHITE/M(90)]</p>
+                  <p>옵션변경</p>
+                </div>
+                <div className="productPrice">
+                  <li className="productPriceTxt">상품금액</li>
+                  <li className="productOnePrice">{price} 원</li>
+                </div>
+                <Count count={count} setCount={setCount} />
+                <div className="delivery">
+                  <li className="deliveryTitle">배송비</li>
+                  <li className="deliveryPrice">무료</li>
+                </div>
+                <div className="cartOneDelete">
+                  <button className="deleteButton" type="reset">
+                    X
+                  </button>
+                </div>
+              </div>
+              <div className="ProductInfo">
+                <input
+                  type="checkbox"
+                  class="cartCheckBox"
+                  checked={checkedState[2]}
+                  onChange={() => handleMonoCheck(2)}
                 />
                 <img
                   className="productImg"
