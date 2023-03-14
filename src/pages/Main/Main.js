c4000221efb921a0bb89074ecd08126e88650b2e
shin <hole546@naver.com>
2023-03-14 17:54:05 +0900

    ADD: 자동 슬라이드 구현 중

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BestProduct from '../../components/BestProduct/BestProduct';
 import NewProduct from '../../components/NewProduct/NewProduct';
 import './Main.scss';
 
 const Main = () => {
+  const [aaa, setAaa] = useState(0);
+  const bbb = () => {
+    if (aaa < MAINIMAGES.length - 1 && aaa >= 0) {
+      setAaa(aaa + 1);
+      console.log(aaa);
+    } else if (aaa === MAINIMAGES.length - 1) {
+      setAaa(0);
+    }
+  };
+
+  setInterval(bbb, 6000);
+
   return (
     <div className="main">
       <div className="wrap">
         <div className="mainImg">
-          <img src="/images/mainSlide1.jpg" alt="반팔 사진" />
-          <img src="/images/mainSlide2.jpg" alt="청바지 사진" />
-          <img src="/images/mainSlide3,jpg" alt="랩퍼 사진" />
+          {MAINIMAGES.map(({ id, img, infor }) => (
+            <img
+              key={id}
+              src={img}
+              alt={infor}
+              style={{
+                transform: `translate(${aaa * -1920}px)`,
+                transition: `2s`,
+              }}
+            />
+          ))}
         </div>
         <BestProduct />
         <NewProduct />
@@ -20,3 +40,9 @@ const Main = () => {
 };
 
 export default Main;
+
+const MAINIMAGES = [
+  { id: 1, img: '/images/mainSlide1.jpg', infor: '반팔 사진' },
+  { id: 2, img: '/images/mainSlide2.jpg', infor: '청바지 사진' },
+  { id: 3, img: '/images/mainSlide3.jpg', infor: '랩퍼 사진' },
+];
