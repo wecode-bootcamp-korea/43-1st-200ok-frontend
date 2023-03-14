57dc68b6376c09d02fc2a79accb7212fcac1548a
shin <hole546@naver.com>
2023-03-14 19:35:36 +0900

    불 필요한 데이터 삭제

--- src/Router.js
+++ src/Router.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
-import SignUp from './pages/SignUp/SignUp';
-import LostAccounts from './pages/LostAccounts/LostAccounts';
-import DetailProduct from './pages/DetailProduct/DetailProduct';
+// import SignUp from './pages/SignUp/SignUp';
 import AllProductList from './pages/AllProductList/AllProductList';
-import Footer from './components/Footer/Footer';
+// import Footer from './components/Footer/Footer';
 
 const Router = () => {
   return (
@@ -16,8 +14,6 @@ const Router = () => {
         <Route path="/main" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<AllProductList />} />
-        <Route path="/lostAccounts" element={<LostAccounts />} />
-        <Route path="/detailProduct" element={<DetailProduct />} />
       </Routes>
       {/* <Footer /> */}
     </BrowserRouter>
