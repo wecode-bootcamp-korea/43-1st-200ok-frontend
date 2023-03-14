d768909d314cf38565efb2ba6c9b1688f2334140
shin <hole546@naver.com>
2023-03-14 16:17:50 +0900

    ADD: 백엔드 연동 확인

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import NavCategory from '../NavCategory/NavCategory';
 import './Nav.scss';
 
 const Nav = () => {
@@ -28,21 +27,17 @@ const Nav = () => {
         </div>
         <div className="topMenu">
           <ul className="topMenuContent">
-            {NAVMENU.map(
-              ({ id, title, division, category, photo, altName }) => (
-                <li className="topMenuContentList" key={id}>
-                  <Link className="topMenuContentLink" to="/allProductList">
-                    {title}
-                  </Link>
-                  <NavCategory
-                    division={division}
-                    category={category}
-                    photo={photo}
-                    altName={altName}
-                  />
-                </li>
-              )
-            )}
+            {NAVMENU.map(({ id, title }) => (
+              <li className="topMenuContentList" key={id}>
+                <Link
+                  className="topMenuContentLink"
+                  to="/allProductList"
+                  state={{ gender: title }}
+                >
+                  {title}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
         <div className="topMember">
@@ -70,46 +65,15 @@ export default Nav;
 
 const NAVMENU = [
   { id: 1, title: '베스트' },
-  {
-    id: 2,
-    title: '우먼',
-    division: ['신상', '베스트', '전체보기'],
-    category: [
-      '아우터',
-      '상의',
-      '하의',
-      '원피스',
-      '액세서리',
-      '이너/언더웨어',
-      '홈웨어/잠옷',
-    ],
-    photo: [
-      '/images/KakaoTalk_20230303_112128342.jpg',
-      '/images/KakaoTalk_20230303_112109123.jpg',
-      '/images/KakaoTalk_20230303_112042004.jpg',
-    ],
-    altName: ['아우터 사진', '상의 사진', '원피스 사진'],
-  },
-  {
-    id: 3,
-    title: '맨',
-    division: ['신상', '베스트', '전체보기'],
-    category: [
-      '아우터',
-      '상의',
-      '하의',
-      '원피스',
-      '액세서리',
-      '이너/언더웨어',
-      '홈웨어/잠옷',
-    ],
-  },
+  { id: 2, title: 'male' },
+  { id: 3, title: 'female' },
   { id: 4, title: '컬러버레이션' },
   { id: 5, title: '키즈' },
   { id: 6, title: '커뮤니티' },
   { id: 7, title: '런칭 캘린더' },
   { id: 8, title: '리뷰랩' },
 ];
+
 const NAVMEMBER = [
   { id: 1, title: 'login', img: '/images/user.png', altName: 'loginLogo' },
   { id: 2, title: 'search', img: '/images/search.png', altName: 'searchLogo' },
