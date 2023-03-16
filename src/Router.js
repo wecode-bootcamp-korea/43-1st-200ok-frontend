d58b6bb969e9b360e3600ec489466d8226a20c3f
shin <hole546@naver.com>
2023-03-16 18:33:19 +0900

    merge중

--- src/Router.js
+++ src/Router.js
@@ -6,6 +6,7 @@ import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
 import LostAccounts from './pages/LostAccounts/LostAccounts';
 import AllProductList from './pages/AllProductList/AllProductList';
+import DetailProduct from './pages/DetailProduct/DetailProduct';
 import Cart from './pages/Cart/Cart';
 import Footer from './components/Footer/Footer';
 
@@ -14,9 +15,12 @@ const Router = () => {
     <BrowserRouter>
       <Nav />
       <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/lostAccounts" element={<LostAccounts />} />
         <Route path="/allproductList" element={<AllProductList />} />
+        <Route path="/detailproduct/:id" element={<DetailProduct />} />
         <Route path="/Cart" element={<Cart />} />
       </Routes>
       <Footer />
