b256f4c16bac86e5d5d37589275c7e31b83568ef
kr_shin <cskr1013@gmail.com>
2023-03-16 18:04:02 +0900

    상세페이지 완료 및 Main Merge 요청

--- src/pages/DetailProduct/DetailProduct.js
+++ src/pages/DetailProduct/DetailProduct.js
@@ -1,54 +1,30 @@
 import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router';
 import { useNavigate, useParams } from 'react-router-dom';
 import './DetailProduct.scss';
 
 const DetailProduct = () => {
-  const [count, setCount] = useState(1);
-  const [color, setColor] = useState([]);
-  const [size, setSize] = useState([]);
-  const [price, setPrice] = useState([]);
-  const [isImages, setIsImages] = useState(true);
-
-  const params = useParams();
   const navigate = useNavigate();
-
+  const params = useParams();
+  const location = useLocation();
+  const { id } = params;
+  const { gender, status, category } = location.state;
+  const [user, setUser] = useState([]);
+  const [isImages, setIsImages] = useState(true);
+  const [count, setCount] = useState(1);
+  const [product, setProduct] = useState({
+    size: '',
+    color: '',
+  });
+  const { size, color } = product;
   const token = localStorage.getItem('token');
-  const colorHeart = 'images/colorheart.png';
-
-  const minusCounter = () => {
-    if (count === 1) {
-      setCount(1);
-    } else {
-      setCount(count - 1);
-    }
-  };
-
-  const plusCounter = () => {
-    if (count === 10) {
-      setCount(10);
-    } else {
-      setCount(count + 1);
-    }
-  };
-
-  const handleSelector = event => {
-    console.log(event.target.value);
-  };
+  const colorHeart = '/images/colorheart.png';
 
   const onClickWishImg = () => {
     setTimeout(() => {
       alert('wish 리스트에 추가하였습니다.');
-      console.log('하트버튼 클릭');
     }, 100);
     return setIsImages(false);
-  };
-
-  const onClickAddCart = () => {
-    if (token) {
-      alert('장바구니에 추가하였습니다.');
-    } else {
-      alert('로그인 해주세요');
-    }
   };
 
   const oderValidation = () => {
@@ -60,105 +36,161 @@ const DetailProduct = () => {
     }
   };
 
-  useEffect(() => {
-    fetch('/data/productPrice.json')
-      .then(res => res.json())
-      .then(data => {
-        setPrice(data);
-      });
-  }, [params.index]);
+  const onClickAddCart = () => {
+    if (token) {
+      fetch(
+        `http://10.58.52.201:3010/carts/post?productId=${id}&size=${size}&color=${color}&token=${token}&quantity=${count}`,
+        {
+          method: 'POST',
+        }
+      )
+        .then(response => response.json())
+        .then(data => {
+          alert('장바구니에 추가하였습니다.');
+          navigate('/cart');
+        });
+    } else {
+      alert('로그인 해주세요');
+      navigate('/login');
+    }
+  };
+
+  const updateProduct = e => {
+    const { value, title } = e.target;
+    setProduct({ ...product, [title]: value });
+  };
 
   useEffect(() => {
-    fetch('/data/colorList.json')
+    fetch(
+      `http://10.58.52.201:3010/products?gender=${gender}&status=${status}&category=${category}&productId=${id}`
+    )
       .then(res => res.json())
-      .then(data => {
-        setColor(data);
-      });
-  }, [params.index]);
+      .then(data => setUser(data.data));
+  }, [id]);
 
   useEffect(() => {
-    fetch('/data/sizeList.json')
-      .then(res => res.json())
-      .then(data => {
-        setSize(data);
-      });
-  }, [params.index]);
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
 
   return (
-    <div className="detailProduct">
-      <section className="productInfo">
-        <div className="imgWrapper">
-          <img
-            className="productImg"
-            src="/images/ziozia_sample.jpg"
-            alt="제품 이미지"
-          />
-        </div>
-      </section>
-      <aside className="infoArea">
-        <div className="productName">[남성 아우터 패딩]</div>
-        <div className="priceArea">
-          <div className="priceArea1">제품 가격 :</div>
-          <div className="priceArea2">{Math.floor(price).toLocaleString()}</div>
-          <div className="priceArea3">할인 가격 :</div>
-          <div className="priceArea4">45,000 원 (10%)</div>
-        </div>
-        <div className="countInput">
-          <button className="minusButton" type="button" onClick={minusCounter}>
-            -1
-          </button>
-          <div className={`productCount${count}`}>수량 : {count}</div>
-          <button className="plusButton" type="button" onClick={plusCounter}>
-            +1
-          </button>
-        </div>
-        <div className="productColor">
-          <p>[Color]</p>
-          {color.map(productColors => {
-            return (
-              <button
-                className="productColor1"
-                key={productColors.id}
-                onClick={handleSelector}
-                value={productColors}
-                style={{ backgroundColor: `${productColors.color}` }}
-              >
-                {productColors.color}
-              </button>
-            );
-          })}
-        </div>
-        <div className="productSize">
-          <p>[Size]</p>
-          {size.map(productSizes => {
-            return (
-              <button className="productSize1" key={productSizes.id}>
-                {productSizes.size}
-              </button>
-            );
-          })}
-        </div>
-        <div className="sumPrice">
-          <p>총 제품금액 : {count}</p>
-        </div>
-        <div className="buttons">
-          <button>
-            <img
-              className="wishImg"
-              src={isImages ? 'images/heart.png' : `${colorHeart}`}
-              alt="wish 이미지"
-              onClick={onClickWishImg}
-            />
-          </button>
-          <button className="cartButton" onClick={onClickAddCart}>
-            장바구니 담기
-          </button>
-          <button className="orderButton" onClick={oderValidation}>
-            주문하기
-          </button>
-        </div>
-      </aside>
-    </div>
+    <>
+      {user.map(
+        ({
+          id,
+          image_url,
+          colors,
+          discount_rate,
+          discounted_price,
+          name,
+          price,
+          sizes,
+        }) => (
+          <div key={id} className="detailProduct">
+            <section className="productInfo">
+              <div className="imgWrapper">
+                <img className="productImg" src={image_url} alt="제품 이미지" />
+              </div>
+            </section>
+            <aside className="infoArea">
+              <div className="productName">{name}</div>
+              <div className="priceArea">
+                <div className="priceArea1">제품 가격 :</div>
+                <div className="priceArea2">
+                  {Math.floor(price).toLocaleString()} 원
+                </div>
+                <div className="priceArea3">할인 가격 :</div>
+                <div className="priceArea4">
+                  {Math.floor(discounted_price).toLocaleString()} 원 (
+                  {discount_rate}%)
+                </div>
+              </div>
+              <div className="countInput">
+                <button
+                  className="minusButton"
+                  type="button"
+                  onClick={() => {
+                    if (count > 1) {
+                      setCount(count - 1);
+                    }
+                  }}
+                >
+                  -1
+                </button>
+                <div className={`productCount${count}`}>수량 : {count}</div>
+                <button
+                  className="plusButton"
+                  type="button"
+                  onClick={() => {
+                    if (count < 10) {
+                      setCount(count + 1);
+                    }
+                  }}
+                >
+                  +1
+                </button>
+              </div>
+              <div className="productColor">
+                <p>[Color]</p>
+                {colors.map(item => (
+                  <button
+                    key={item}
+                    className="productColor1"
+                    title="color"
+                    value={item}
+                    onClick={updateProduct}
+                    style={{ backgroundColor: { item } }}
+                  >
+                    {item.toUpperCase()}
+                  </button>
+                ))}
+              </div>
+              <div className="productSize">
+                <p>[Size]</p>
+                {sizes.map(item => (
+                  <button
+                    key={item}
+                    className="productSize1"
+                    title="size"
+                    value={item}
+                    onClick={updateProduct}
+                  >
+                    {item.toUpperCase()}
+                  </button>
+                ))}
+              </div>
+              <div className="sumPrice">
+                <p>
+                  선택 옵션 : {color.toUpperCase()} / {size.toUpperCase()} /
+                  {count}
+                </p>
+              </div>
+              <div className="sumPrice">
+                <p>
+                  총 제품금액 : ({(discounted_price * count).toLocaleString()}
+                  원)
+                </p>
+              </div>
+              <div className="buttons">
+                <button>
+                  <img
+                    className="wishImg"
+                    src={isImages ? '/images/heart.png' : `${colorHeart}`}
+                    alt="wish 이미지"
+                    onClick={onClickWishImg}
+                  />
+                </button>
+                <button className="cartButton" onClick={onClickAddCart}>
+                  장바구니 담기
+                </button>
+                <button className="orderButton" onClick={oderValidation}>
+                  주문하기
+                </button>
+              </div>
+            </aside>
+          </div>
+        )
+      )}
+    </>
   );
 };
 
